08b8b26c3e1f7a9434f870d61b2f7c785cae4c3b
Artur Cepuc <arturcepuc@gmail.com>
2023-01-26 19:27:43 +0200

    v2

--- ssh-helper.py
+++ ssh-helper.py
@@ -5,11 +5,12 @@ client = paramiko.SSHClient() # Create an SSH client
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # Add the host's ssh key automatically
 
 
+hostname = ''
 username = 'ec2-user' # AWS EC2 default username 
 key_filename = '' # path to your key
 password = ''
 
-client.connect(hostname='35.180.208.17', username='ec2-user', key_filename=key_filename) # you can use password inseat key_filename
+client.connect(hostname=hostname, username=username, key_filename=key_filename) # you can use password inseat key_filename
 
 
 stdin, stdout, stderr = client.exec_command('date')
