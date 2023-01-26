388dbe2c8f1a0883ff33f2c17c1a33c302c5c0dd
Artur Cepuc <arturcepuc@gmail.com>
2023-01-26 21:11:00 +0200

    Add inventory file and possibility to ssh into multiple vms

--- ssh-helper.py
+++ ssh-helper.py
@@ -1,21 +1,35 @@
 import paramiko
 
 
-client = paramiko.SSHClient() # Create an SSH client
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # Add the host's ssh key automatically
+def populate_hosts(inventory):
+    hosts_list = []
+    with open(inventory, "r") as inventory_file:
+        for host in inventory_file:
+            host  = host.rstrip("\n")
+            hosts_list.append(host)
+    return hosts_list
 
+def command(client):
+    stdin, stdout, stderr = client.exec_command('hostname')
+    print(stdout.read().decode())
 
-hostname = ''
-username = 'ec2-user' # AWS EC2 default username 
-key_filename = '' # path to your key
-password = ''
+def command2(client):
+    stdin, stdout, stderr = client.exec_command('date')
+    print(stdout.read().decode())
 
-client.connect(hostname=hostname, username=username, key_filename=key_filename) # you can use password inseat key_filename
+if __name__ == "__main__":
 
+    client = paramiko.SSHClient() # Create an SSH client
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # Add the host's ssh key automatically
 
-stdin, stdout, stderr = client.exec_command('date')
-print(stdout.read().decode())
+    hosts = populate_hosts("hosts")
+    username = 'ec2-user' # AWS EC2 default username 
+    key_filename = '' # path to your key
+    password = ''
 
-# Close the connection
-client.close()
+    for host in hosts:
+        client.connect(hostname=host, username=username, key_filename=key_filename) # you can use password inseat key_filename
+        command(client)
+        command2(client)
+        client.close()
 
